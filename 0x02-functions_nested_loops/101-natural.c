2ad61731fd175f50a45fe1f54e63c07746961f4e
abdulmalik-source <abdulmaliky223@gmail.com>
2022-03-16 07:41:44 -0700

    Sum of multiples

--- 0x02-functions_nested_loops/101-natural.c
+++ 0x02-functions_nested_loops/101-natural.c
@@ -1,11 +1,10 @@
 #include <stdio.h>
 
 /**
- * main - computes and prints all the multiples of 3 or 5 before 1024
- *
+ * main - computes and prints the sum of all the multiples
+ * of 3 or 5 below 1024
  * Return: Always 0 (Success)
  */
-
 int main(void)
 {
 	unsigned long int sum3, sum5, sum;
@@ -14,14 +13,13 @@ int main(void)
 	sum3 = 0;
 	sum5 = 0;
 	sum = 0;
-	
-	for (i = 0; i < 1024; ++1)
+
+	for (i = 0; i < 1024; ++i)
 	{
 		if ((i % 3) == 0)
 		{
 			sum3 = sum3 + i;
-		}
-		else if ((i % 5) == 0)
+		} else if ((i % 5) == 0)
 		{
 			sum5 = sum5 + i;
 		}
