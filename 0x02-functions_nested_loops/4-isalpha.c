3ab43c66f4f2151584b323c714c1954ff1dfdc7d
abdulmalik-source <abdulmaliky223@gmail.com>
2022-03-16 03:07:24 -0700

    Check if letter is lowercase or uppercase

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -1,12 +1,10 @@
 #include "main.h"
-
 /**
  * _isalpha - checks if character is alphabet
  * @c: c is an ascii character
  *
  * Return: 1 (if letter)
  */
-
 int _isalpha(int c)
 {
 	if (c >= 'a' && c <= 'z')
