2795200699123c18fb63c3a24882488e5fd1f53c
abdulmalik-source <abdulmaliky223@gmail.com>
2022-04-26 09:21:20 -0700

    Advanced tasks on more singly linked lists

--- 0x13-more_singly_linked_lists/100-reverse_listint.c
+++ 0x13-more_singly_linked_lists/100-reverse_listint.c
@@ -1,42 +1,27 @@
 #include "lists.h"
 
 /**
- * find_listint_loop - finds the loop in a linked list.
+ * reverse_listint - reverses a linked list.
  * @head: head of a list.
  *
- * Return: the address of the node where the loop starts.
+ * Return: pointer to the first node.
  */
-listint_t *find_listint_loop(listint_t *head)
+listint_t *reverse_listint(listint_t **head)
 {
-	listint_t *p2;
-	listint_t *prev;
+	listint_t *p;
+	listint_t *n;
 
-	p2 = head;
-	prev = head;
-	while (head && p2 && p2->next)
+	p = NULL;
+	n = NULL;
+
+	while (*head != NULL)
 	{
-		head = head->next;
-		p2 = p2->next->next;
-
-		if (head == p2)
-		{
-			head = prev;
-			prev =  p2;
-			while (1)
-			{
-				p2 = prev;
-				while (p2->next != head && p2->next != prev)
-				{
-					p2 = p2->next;
-				}
-				if (p2->next == head)
-					break;
-
-				head = head->next;
-			}
-			return (p2->next);
-		}
+		n = (*head)->next;
+		(*head)->next = p;
+		p = *head;
+		*head = n;
 	}
 
-	return (NULL);
+	*head = p;
+	return (*head);
 }
